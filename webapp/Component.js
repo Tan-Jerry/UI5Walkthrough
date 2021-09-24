00a2a0fd908dafa8e5322170ada0e91c4c612e12
jerry <tp8810@126.com>
2021-09-24 17:45:09 +0800

    Step 10 Descriptor

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,19 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/resource/ResourceModel"
-], function(UIComponent, JSONModel, ResourceModel) {
+	"sap/ui/model/json/JSONModel"
+], function(UIComponent, JSONModel) {
 	"use strict";
 
 	const theClass = UIComponent.extend("sap.ui.learning.Component", {
 		metadata: {
-			"interfaces": ["sap.ui.core.IAsyncContentCreation"],
-			"rootView": {
-			   "viewName": "sap.ui.learning.view.App",
-			   "type": "XML",
-			   /*"async": true, // implicitly set via the sap.ui.core.IAsyncContentCreation interface*/
-			   "id": "app"
-			}
+			interfaces: ["sap.ui.core.IAsyncContentCreation"],
+			manifest: "json"
 		}
 	});
 
@@ -27,11 +21,6 @@ sap.ui.define([
 		};
 		let oModel = new JSONModel(oData);
 		this.setModel(oModel);
-
-		let i18nModel = new ResourceModel({
-			bundleName: "sap.ui.learning.i18n.i18n"
-		});
-		this.setModel(i18nModel, "i18n");
 	};
 
 	return theClass;
